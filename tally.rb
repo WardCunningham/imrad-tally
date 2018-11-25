eb48c4e49d69f2486e10ce8d58772d7edde4afc2
Ward Cunningham <ward@c2.com>
2018-11-25 15:20:39 -0800

    recurse following links

--- tally.rb
+++ tally.rb
@@ -1,13 +1,40 @@
 require 'json'
 
 @root = 'imrad.ward.asia.wiki.org'
+@sitemap = JSON.parse `curl -s #{@root}/system/sitemap.json`
 
 @todo = ["welcome-visitors"]
 @done = []
 
-def visit slug
-  page = JSON.parse `curl -s #{@root}/#{slug}.json`
-  puts page['story'][0]['text']
+@start = Time.at 1543178737
+@counts = Hash.new 0
+@counts['minutes'] = ((Time.now - @start) / 60).floor
+
+def asSlug title
+  title.gsub(/\s/, '-').gsub(/[^A-Za-z0-9-]/, '').downcase
 end
 
-visit @todo.pop
+def todo title
+  slug = asSlug title
+  return if @todo.include? slug or @done.include? slug
+  return unless @sitemap.find {|page| page['slug']==slug}
+  @todo.push slug
+end
+
+def visit slug
+  @done.push slug
+  page = JSON.parse `curl -s #{@root}/#{slug}.json`
+  @counts['pages'] += 1
+  page['story'].each do |item|
+    @counts['items'] += 1
+    @counts["type #{item['type']}"] += 1
+    next if item['text'].nil?
+    item['text'].scan(/\[\[(.+?)\]\]/) do |title|
+      @counts['links'] += 1
+      todo title[0]
+    end
+  end
+end
+
+visit @todo.pop until @todo.empty?
+puts JSON.pretty_generate @counts.sort.to_h
